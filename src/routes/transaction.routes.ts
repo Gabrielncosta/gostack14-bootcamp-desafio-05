61aacacbd62973975915ebd8e18bd4b2647b644b
Gabriel <gabrielncosta208@gmail.com>
2020-12-06 09:44:21 -0300

    feat(routes): list transactions

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import ListTransactionsService from '../services/ListTransactionsService';
+import GetBalanceService from '../services/GetBalanceService';
 
 const transactionRouter = Router();
 
@@ -14,9 +15,13 @@ transactionRouter.get('/', (request, response) => {
       transactionsRepository,
     );
 
-    const transaction = listTransactionsService.execute();
+    const transactions = listTransactionsService.execute();
 
-    return response.json(transaction);
+    const getBalanceService = new GetBalanceService(transactionsRepository);
+
+    const balance = getBalanceService.execute();
+
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -29,6 +34,14 @@ transactionRouter.post('/', (request, response) => {
     const createTransactionService = new CreateTransactionService(
       transactionsRepository,
     );
+
+    const getBalanceService = new GetBalanceService(transactionsRepository);
+
+    const balance = getBalanceService.execute();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw new Error('Invalid value');
+    }
 
     const transaction = createTransactionService.execute({
       title,
